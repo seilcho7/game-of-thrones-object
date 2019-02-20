17f2108db4e878d13ac949b75bf96d7fd733950c
Seil Cho <seilcho7@hotmail.com>
2019-02-20 11:59:12 -0500

    adds ability to greet another Character

--- tests.py
+++ tests.py
@@ -17,6 +17,6 @@ arya.inventory.append('mask')
 print(len(arya.inventory))
 
 # arya should have a 'greet' method 
-# and when I call it, it should return
-# "Hello, I am Arya Stark. I am awesome."
-print(arya.greet())
+# and when I call with 'arya.greet(jon)', it should return
+# "Hello, Jon Snow, I am Arya Stark. I am awesome."
+print(arya.greet(jon))
