de168f3c00a99989c0f46f1555b333249c5b07b3
Seil Cho <seilcho7@hotmail.com>
2019-02-20 11:20:56 -0500

    adds the avatar attribute and tests it

--- character.py
+++ character.py
@@ -7,8 +7,8 @@
 
 class Character():
     # "dunder" init method is the constructor
-    def __init__(self, new_name):
+    def __init__(self, new_name, new_avatar):
         # "self" is the customary way to refer to the instance being built
         # in some other languages, they use "this"
         self.name = new_name
-    
+        self.avatar = new_avatar
---
+++ tests.py
@@ -0,0 +1,9 @@
+from character import Character
+
+# Characters can be instantiated with name and avatar
+
+arya = Character("Arya Stark", "arya.png")
+jon = Character("Jon Snow", "jon.png")
+
+print(arya.name, arya.avatar)
+print(jon.name, jon.avatar)
