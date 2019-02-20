20f58e413145a9517e2912aabd2999b95926ee4e
Seil Cho <seilcho7@hotmail.com>
2019-02-20 12:09:09 -0500

    adds ability to greet either nobody or a Character

--- tests.py
+++ tests.py
@@ -20,3 +20,9 @@ print(len(arya.inventory))
 # and when I call with 'arya.greet(jon)', it should return
 # "Hello, Jon Snow, I am Arya Stark. I am awesome."
 print(arya.greet(jon))
+
+# arya should have a 'greet' method 
+# and when I call with 'arya.greet()', it should return
+# "Hello, I am Arya Stark. I am awesome."
+print(arya.greet())
+
